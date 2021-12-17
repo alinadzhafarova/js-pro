f304d202a1f663db532b03fdadf8dc70653b5179
Alina Dzhafarova <atlazdzhafarova69@gmail.com>
2021-12-17 12:14:05 +0200

    add carousel

--- carousel with video/carousel.js
+++ carousel with video/carousel.js
@@ -1,27 +1,25 @@
 let urlAPI = 'https://itunes.apple.com/search';
-let $carousel = $('#carousel');
+let $carousel = $('#carousel')
 let $carouselWrapper = $carousel.find('.carousel-inner');
 
-// disable autoplay for the carousel
-$carousel.carousel({interval: 0});
+$carousel.carousel({interval: 0})
 
-// add event on click 'previous' button of carousel
 $carousel.on('click', '.carousel-control-prev', function (e) {
     e.preventDefault();
-    $carousel.carousel('prev');
+    $carousel.carousel('prev')
     $carouselWrapper.find('video').trigger('pause');
     setTimeout(() => $carouselWrapper.find('.active video').trigger('play'), 1000);
 });
 
-// add event on click 'next' button of carousel
+
 $carousel.on('click', '.carousel-control-next', function (e) {
     e.preventDefault();
-    $carousel.carousel('next');
+    $carousel.carousel('next')
     $carouselWrapper.find('video').trigger('pause');
     setTimeout(() => $carouselWrapper.find('.active video').trigger('play'), 1000);
 });
 
-// add event on click 'submit' button of search form
+
 $('#search-form').on('submit', function (e) {
     e.preventDefault();
     let query = $(this).find('#search-term').val();
@@ -29,16 +27,17 @@ $('#search-form').on('submit', function (e) {
     getVideo(urlAPI, query);
 });
 
-// make ajax request
+
 function getVideo(url, term) {
     $.ajax({
         url,
         method: 'GET',
         dataType: 'json',
-        data: `limit=10&entity=musicVideo&term=${term}`,
+        data:(`limit=10&entity=musicVideo&term= ${term}`),
         success(data) {
             data.results.forEach(item => createSlide($carouselWrapper, item));
-            $carouselWrapper.find('.carousel-item').first().addClass('active');
+            $carouselWrapper.find('.carousel-item')
+                .first().addClass('active');
         },
         error(error) {
             console.log(error)
@@ -46,7 +45,7 @@ function getVideo(url, term) {
     })
 }
 
-// create new carousel item element
+
 function createSlide(carousel, data) {
     let slide = $('<div></div>')
         .addClass('carousel-item')
@@ -56,5 +55,6 @@ function createSlide(carousel, data) {
         .attr('controls', 'controls')
         .attr('src', data.previewUrl)
         .attr('poster', data.artworkUrl)
+
         .appendTo(slide);
 }
--- game/game.js
+++ game/game.js
@@ -1,44 +1,78 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let figureArr = [];
-let intervalId = null;
+'use strict';
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
 
-function Figure(ctx) {
-    this.x = Math.floor(0 + Math.random() * (canvas.width + 1 - 0));
-    this.y = 0;
-    this.width = 20;
-    this.height = 20;
-    this.color = '#' + Math.random().toString(16).substr(-6);
-    this.speed = 1;
-    ctx.fillStyle = this.color;
-    this.drow = () => {
-        ctx.fillRect(this.width + this.x, this.y, this.width, this.height);
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+
+function Square(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+
+    this.r = Math.round(Math.random() * 255);
+    this.g = Math.round(Math.random() * 255);
+    this.b = Math.round(Math.random() * 255);
+    this.rgba = "rgba(" + this.r + ", " + this.g + ", " + this.b + ", 1)";
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.fillStyle = this.rgba;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.y += this.speed;
+        ctx.closePath();
     };
 }
 
+function createGame() {
+    let x = Math.random() * 620;
+    let speed = Math.random() * 0.7 + 0.3;
+    let gameSquare = new Square(x, -20, 20, 20, speed);
+
+    arraySquares.push(gameSquare);
+    document.querySelector('#score').textContent = 	score;
+}
+
+let arraySquares = [];
+let score = 0;
+
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    figureArr.forEach(item => {
-        ctx.fillStyle = item.color;
-        item.drow();
-        item.y += item.speed;
-    });
 
+    arraySquares = arraySquares.filter((element) => {
+        element.draw();
+        return element.y < canvas.width;
+    });
     requestAnimationFrame(animate);
 }
 
-document.getElementById('startBtn').addEventListener('click', event => {
-    figureArr.push(new Figure(ctx));
-    animate();
+let infinity;
 
-    intervalId = setInterval(function () {
-        figureArr.push(new Figure(ctx));
-        animate();
-    }, 100);
+btnStart.addEventListener('click', () => {
+    if (arraySquares.length === 0) {
+        let randomInterval = Math.random() * 1200 + 200;
+        infinity = setInterval(createGame, randomInterval);
+        score = 0;
+    }
 });
 
-document.getElementById('stopBtn').addEventListener('click', event => {
-    clearInterval(intervalId);
-    figureArr = [];
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-})
+btnStop.addEventListener('click', () => {
+    arraySquares = [];
+    clearInterval(infinity);
+});
+
+canvas.addEventListener('click', (event) => {
+    for (let square of arraySquares) {
+        if (square.x <= event.offsetX &&
+            square.y <= event.offsetY &&
+            (square.height + square.x) >= event.offsetX &&
+            (square.width + square.y) >= event.offsetY) {
+            arraySquares.splice(arraySquares.indexOf(square), 1);
+            score += 1;
+        }
+    }
+});
+
+document.body.onload = animate;
