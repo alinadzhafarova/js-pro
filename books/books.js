b2d4d925efd136cde17a13642ee0ce76c924021b
Alina Dzhafarova <atlazdzhafarova69@gmail.com>
2021-12-17 09:12:40 +0200

    add books

--- books/books.js
+++ books/books.js
@@ -1,15 +1,69 @@
 const $form = $('#search-form');
+const $list = $('#book-list');
+const $currentBook = $('#current-book')
+let books = [];
 
-const getBooks = ( text ) => {
-    $.get('https://www.googleapis.com/books/v1/volumes',{ q: text })
-        .done((response) => console.log("response",response.items))
-        .fail((error) => console.log("error",error));
-}
+$currentBook.hide();
+console.log("$currentBook", $currentBook);
 
-$form.on('submit',(event) => {
+const createContent = (data) => {
+    $list.empty()
+    books = data;
+
+
+    data.forEach((item) => {
+        $('<li>')
+            .append(
+                $('<a href="">').text(item.volumeInfo.title)
+            )
+            .attr('data-id', item.id)
+            .addClass('list-group-item')
+            .appendTo($list)
+    });
+};
+
+const getBooks = (text) => {
+    $.get('https://www.googleapis.com/books/v1/volumes', {q: text})
+        .done((response) => createContent(response.items))
+        .fail((error) => console.log("error", error));
+};
+
+$form.on('submit', (event) => {
     event.preventDefault();
     const text = $('input').val()
-    if(text.trim()) {
-        getBooks(text)
+    if (text.trim()) {
+        getBooks(text);
     }
 });
+
+$list.on('click', '[data-id]', function (event) {
+    event.preventDefault();
+    const bookId = $(this).data('id');
+    const {volumeInfo} = books.find((item) => item.id === bookId)
+    $currentBook.fadeIn()
+    $currentBook
+        .find('.book-title')
+        .text(`${volumeInfo.title} | ${volumeInfo.authors.join(", ")} (${volumeInfo.publishedDate})`)
+
+    const $bookBody = $currentBook.find('.book-body')
+    $bookBody.empty();
+
+    $('<img>').attr('src', volumeInfo.imageLinks.thumbnail)
+        .appendTo($bookBody);
+
+    $('<p>').text(volumeInfo.description)
+        .appendTo($bookBody);
+
+    $('<a>').attr('href', volumeInfo.previewLink)
+        .attr('target', '_blank')
+        .text('Read more ...')
+        .addClass('float-right')
+        .appendTo($bookBody);
+})
+
+
+
+
+
+
+
